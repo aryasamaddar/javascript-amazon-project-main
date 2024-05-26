3f7f12cbf32989f496b4a694e5e06dbcae2c23c4
aryasamaddar <bhattagublu@gmail.com>
2024-05-26 22:52:52 +0530

    move add to cart into cart.js

--- Scripts/Amazon.js
+++ Scripts/Amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 let productsHTML='';
 
@@ -54,34 +54,24 @@ products.forEach((product) => {
     </div>
     `;
 })
-
 document.querySelector('.js-products-grid').
 innerHTML = productsHTML;
+
+function updateCartQuantity(){
+    let cartQuantity =0;
+    cart.forEach((item) =>{
+        cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerText = `${cartQuantity}`;
+}
+
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
     button.addEventListener('click',() =>{
         const productId = button.dataset.productId;
+        addToCart(productId);
 
-        let matchingItem ;
-        cart.forEach((item) =>{
-            if(productId == item.productId){
-                matchingItem = item;
-            }
-        })
-
-        if(matchingItem){
-            matchingItem.quantity ++;
-        }else{
-            cart.push({
-                productId: productId,
-                quantity: 1
-            })
-        };
-        let cartQuantity =0;
-        cart.forEach((item) =>{
-            cartQuantity += item.quantity;
-        });
-        document.querySelector('.js-cart-quantity').innerText = `${cartQuantity}`;
+        updateCartQuantity();
     })
 })
 
